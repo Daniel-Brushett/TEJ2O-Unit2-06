17472d3ea65876c854bde4085b76d19b441f3b46
Daniel-Brushett <dj.brushett@stu.ocsb.ca>
2026-02-26 13:00:43 -0500

    Added code, fixed creation date, added creator name

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Daniel Brushett
+ * Created on: 02/26/26
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Effect A
+input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// Effect B
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
